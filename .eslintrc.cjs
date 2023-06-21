0a5cb734886d52e4db7c3de3b65ccba28d18f206
Zak Nesler <zak@nesler.dev>
2023-06-21 11:24:45 -0400

    wip

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,35 +1,71 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const path = require("path");
-
 /** @type {import("eslint").Linter.Config} */
 const config = {
-  overrides: [
-    {
-      extends: [
-        "plugin:@typescript-eslint/recommended-requiring-type-checking",
-      ],
-      files: ["*.ts", "*.tsx"],
-      parserOptions: {
-        project: path.join(__dirname, "tsconfig.json"),
-      },
-    },
-  ],
-  parser: "@typescript-eslint/parser",
+  root: true,
+  env: { node: true, browser: true, es6: true, commonjs: true },
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: path.join(__dirname, "tsconfig.json"),
+    tsconfigRootDir: __dirname,
+    ecmaFeatures: { jsx: true },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-  plugins: ["@typescript-eslint"],
-  extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
+  extends: [
+    'prettier',
+    'plugin:import/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  plugins: ['react', 'prettier', '@typescript-eslint', 'react-refresh'],
   rules: {
-    "@typescript-eslint/consistent-type-imports": [
-      "warn",
+    'react-refresh/only-export-components': 'warn',
+    'import/default': 'error',
+    'import/named': 'error',
+    'import/newline-after-import': ['error', { count: 1 }],
+    'import/no-unresolved': 'error',
+    'import/order': [
+      'error',
       {
-        prefer: "type-imports",
-        fixStyle: "inline-type-imports",
+        groups: [
+          ['external', 'builtin'],
+          ['internal', 'parent', 'index'],
+          'sibling',
+        ],
+        alphabetize: { order: 'asc', caseInsensitive: true },
       },
     ],
-    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    'no-nested-ternary': 0,
+    'no-underscore-dangle': 0,
+    'no-unused-vars': 'off',
+    'react/no-unknown-property': 'warn',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/consistent-type-imports': [
+      'warn',
+      {
+        prefer: 'type-imports',
+        fixStyle: 'inline-type-imports',
+      },
+    ],
+    'prettier/prettier': [
+      'error',
+      {
+        printWidth: 80,
+        arrowParens: 'avoid',
+        semi: false,
+        singleQuote: true,
+        tabWidth: 2,
+        trailingComma: 'all',
+        endOfLine: 'lf',
+      },
+    ],
   },
-};
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {},
+    },
+  },
+}
 
-module.exports = config;
+module.exports = config
--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -1,12 +1,12 @@
-import { z } from "zod";
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { z } from 'zod'
+import { createTRPCRouter, publicProcedure } from '~/server/api/trpc'
 
 export const exampleRouter = createTRPCRouter({
   hello: publicProcedure
-    .input(z.object({ text: z.string() }))
+    .input(z.object({ title: z.string() }))
     .query(({ input }) => {
       return {
-        greeting: `Hello ${input.text}`,
-      };
+        title: input.title,
+      }
     }),
-});
+})
