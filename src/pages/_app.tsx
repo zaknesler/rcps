329f3d4ee06878341b2e3443abae4010d751a007
Zak Nesler <zak@nesler.dev>
2023-06-22 09:52:52 -0400

    add viewport meta tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -18,6 +18,7 @@ const MyApp: AppType = ({ Component, pageProps }) => {
     <>
       <Head>
         <title>rcps</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta name="description" content="recipes" />
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
       </Head>
