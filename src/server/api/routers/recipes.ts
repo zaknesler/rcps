3049dfbf89ffbdba0b3038467d73d4a1c798cbdf
Zak Nesler <zak@nesler.dev>
2023-06-29 13:09:21 -0400

    rm create for now

--- src/server/api/routers/recipes.ts
+++ src/server/api/routers/recipes.ts
@@ -53,25 +53,25 @@ export const recipeRouter = router({
       }),
     ),
 
-  create: procedures.auth
-    .input(
-      z.object({
-        slug: z.string(),
-        title: z.string(),
-        summary: z.string(),
-        ingredients: z.array(
-          z.object({
-            amount: z.string(),
-            name: z.string(),
-            prep: z.string().optional(),
-            note_id: z.string().optional(),
-          }),
-        ),
-        steps: z.array(z.object({ text: z.string() })),
-        notes: z.array(z.object({ id: z.string(), text: z.string() })),
-      }),
-    )
-    .mutation(async ({ input, ctx }) =>
-      ctx.prisma.recipe.create({ data: input }),
-    ),
+  // create: procedures.auth
+  //   .input(
+  //     z.object({
+  //       slug: z.string(),
+  //       title: z.string(),
+  //       summary: z.string(),
+  //       ingredients: z.array(
+  //         z.object({
+  //           amount: z.string(),
+  //           name: z.string(),
+  //           prep: z.string().optional(),
+  //           note_id: z.string().optional(),
+  //         }),
+  //       ),
+  //       steps: z.array(z.object({ text: z.string() })),
+  //       notes: z.array(z.object({ id: z.string(), text: z.string() })),
+  //     }),
+  //   )
+  //   .mutation(async ({ input, ctx }) =>
+  //     ctx.prisma.recipe.create({ data: input }),
+  //   ),
 })
